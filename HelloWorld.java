2e0df9c3ef2e48ed3415e21c36478adc588384e9
Ajzal Mohammed Hussain <b00082911@aus.edu>
2023-01-31 18:18:37 +0400

    Feature B added

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,11 @@
-package cmp420l;
+package ajzalmh10.COE420L1;
 
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+    }
+    public static void featureB()
+    {
+    	System.out.println("Hello, World! from Feature B");
     }
 }
